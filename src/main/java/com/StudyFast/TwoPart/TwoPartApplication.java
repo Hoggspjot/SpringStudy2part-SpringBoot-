c63e72dd8be3601cdf60e1338045d080ca823e99
Hoggspjot <vingisaar@gmail.com>
2025-09-17 23:40:23 +0300

    initial commit

---
+++ src/main/java/com/StudyFast/TwoPart/TwoPartApplication.java
@@ -0,0 +1,13 @@
+package com.StudyFast.TwoPart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoPartApplication.class, args);
+	}
+
+}
